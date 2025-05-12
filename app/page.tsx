99759a22a0894659fdc67c787cff6a8dbc8ef81a
zonyra-hubert <danielshubert9@gmail.com>
2025-05-12 09:20:10 +0000

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default async function Home() {
   });
 
   return (
-    <main className="">
+    <main className="mb-5">
       <Algolia />
       <ProductTags />
       <Products variants={data} />
